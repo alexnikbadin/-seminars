3fbcab079bdede046e6a7cbebb8d48caddcad757
Alex.B <badinalexander@icloud.com>
2023-12-26 10:38:20 +0300

    The quad of num

---
+++ seminar_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*System.Console.WriteLine("Hello");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(num);*/
+
+//напишите программу которая на вход приниает два целых числа и проверяет является ли 1 квадратом второго
+
+/*System.Console.WriteLine("Input first num: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second num: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 / num2 == num1)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}*/
+
