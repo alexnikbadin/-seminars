25a82b302dc3dd8517dc9385791c844bee913c27
Alex.B <badinalexander@icloud.com>
2024-01-13 13:34:29 +0300

    Задача 1

---
+++ Homework_sem_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+void division(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+division(number);
